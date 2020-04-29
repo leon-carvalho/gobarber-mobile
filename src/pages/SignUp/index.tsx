738d4503cd2da0c8427936b435af09538b34d691
leon-carvalho <leonardoscc15@gmail.com>
2020-04-29 18:08:58 -0300

    Styling SignUp page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
 
-const SignUp: React.FC = () => <Container />;
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+
+const SignUp: React.FC = () => (
+  <>
+    <KeyboardAvoidingView
+      enabled
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Container>
+          <Image source={logoImg} />
+
+          <View>
+            <Title>Faça seu logon</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => console.log('empty')}>Crie sua conta</Button>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+    <BackToSignIn>
+      <Icon name="arrow-left" size={20} color="#fff" />
+      <BackToSignInText>Voltar para logon</BackToSignInText>
+    </BackToSignIn>
+  </>
+);
 
 export default SignUp;
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,50 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const Title = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  margin: 64px 0 24px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  background: #312e38;
+  border-top-width: 1px;
+  border-top-color: #232129;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  margin-left: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
