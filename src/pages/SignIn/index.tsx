4610644bbd5898cf30015ba3691f616afe8e0037
leon-carvalho <leonardoscc15@gmail.com>
2020-04-29 14:04:18 -0300

    Aplying custom font into SignIn page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,11 +3,13 @@ import { Image } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container } from './styles';
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => (
   <Container>
     <Image source={logoImg} />
+
+    <Title>Faça seu logon</Title>
   </Container>
 );
 
