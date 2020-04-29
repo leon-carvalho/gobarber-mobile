fe447010919d9719d477b6b13339c886883d2672
leon-carvalho <leonardoscc15@gmail.com>
2020-04-29 18:08:36 -0300

    Updating SignIn page adding padding bottom on Android to show button

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import styled from 'styled-components/native';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
 
@@ -6,7 +7,7 @@ export const Container = styled.View`
   align-items: center;
   justify-content: center;
 
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
 export const Title = styled.Text`
