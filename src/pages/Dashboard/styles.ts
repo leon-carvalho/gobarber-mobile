5061e9c44af299c47bbc7a3d67338d1f578b4a13
leon-carvalho <leonardoscc15@gmail.com>
2020-06-25 11:35:35 -0300

    feat: getting providers from API

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import { Provider } from './index';
 
 export const Container = styled.View``;
 
@@ -35,5 +39,57 @@ export const UserAvatar = styled.Image`
   width: 58px;
   height: 58px;
   border-radius: 28px;
-  background-color: #c4cdc8;
+  background-color: #c4cdc811;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px 16px;
+`;
+
+export const ProvidersListTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 24px;
+  margin-bottom: 24px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProviderContainer = styled(RectButton)`
+  padding: 20px;
+  margin-bottom: 16px;
+  border-radius: 10px;
+  background-color: #3e3b47;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+  background-color: #c4cdc811;
+`;
+
+export const ProviderInfo = styled.View`
+  flex: 1;
+  margin-left: 20px;
+`;
+
+export const ProviderName = styled.Text`
+  color: #f4ede8;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProviderMeta = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+  margin-top: 8px;
+`;
+
+export const ProviderMetaText = styled.Text`
+  color: #999591;
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Regular';
 `;
